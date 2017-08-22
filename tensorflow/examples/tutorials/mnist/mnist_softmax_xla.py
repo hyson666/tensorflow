633010a3d51a9fbdb137223b53d98c1c3cad1704
hyson666 <601852941@qq.com>
2017-08-22 10:56:03 +0800

    添加注释

--- tensorflow/examples/tutorials/mnist/mnist_softmax_xla.py
+++ tensorflow/examples/tutorials/mnist/mnist_softmax_xla.py
@@ -12,9 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Simple MNIST classifier example with JIT XLA and timelines.
 
+"""A very simple MNIST classifier.
+See extensive documentation at
+https://www.tensorflow.org/get_started/mnist/beginners
 """
+
+#里仅仅使用了简单的softmax回归来实现手写数字Ocr，
+#可以从文档中看到是只利用了输入和输出结合Softmax函数进行训练，抽象理解为无隐藏层的神经网络
+
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -22,23 +29,22 @@ from __future__ import print_function
 import argparse
 import sys
 
+from tensorflow.examples.tutorials.mnist import input_data
+
 import tensorflow as tf
 
-from tensorflow.examples.tutorials.mnist import input_data
-from tensorflow.python.client import timeline
-
 FLAGS = None
-
 
 def main(_):
   # Import data
   mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
   # Create the model
+  # None说明第一维度可以为任意长度，即样例数目不确定
   x = tf.placeholder(tf.float32, [None, 784])
-  w = tf.Variable(tf.zeros([784, 10]))
+  W = tf.Variable(tf.zeros([784, 10]))
   b = tf.Variable(tf.zeros([10]))
-  y = tf.matmul(x, w) + b
+  y = tf.matmul(x, W) + b
 
   # Define loss and optimizer
   y_ = tf.placeholder(tf.float32, [None, 10])
@@ -52,56 +58,31 @@ def main(_):
   #
   # So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
   # outputs of 'y', and then average across the batch.
+  # 用交叉熵定义的代价函数，目标就是使其最小化,这里已经封装好了，说明见到上面
   cross_entropy = tf.reduce_mean(
       tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+  # 利用学习速率为0.5的梯度下降进行最优化计算，定义每一步是最小化这个代价函数
   train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-  config = tf.ConfigProto()
-  jit_level = 0
-  if FLAGS.xla:
-    # Turns on XLA JIT compilation.
-    jit_level = tf.OptimizerOptions.ON_1
-
-  config.graph_options.optimizer_options.global_jit_level = jit_level
-  run_metadata = tf.RunMetadata()
-  sess = tf.Session(config=config)
-  tf.global_variables_initializer().run(session=sess)
-  # Train
-  train_loops = 1000
-  for i in range(train_loops):
+  # 启动这个对话，准备开始训练过程
+  sess = tf.InteractiveSession()
+  tf.global_variables_initializer().run()
+  # Train（每次从训练集中随机抓取100个样例进行训练，而不是利用全部数据，这样效率更高）
+  # 最后得到判断矩阵y，还有W、b的值（是向量）
+  for _ in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
-
-    # Create a timeline for the last loop and export to json to view with
-    # chrome://tracing/.
-    if i == train_loops - 1:
-      sess.run(train_step,
-               feed_dict={x: batch_xs,
-                          y_: batch_ys},
-               options=tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE),
-               run_metadata=run_metadata)
-      trace = timeline.Timeline(step_stats=run_metadata.step_stats)
-      with open('timeline.ctf.json', 'w') as trace_file:
-        trace_file.write(trace.generate_chrome_trace_format())
-    else:
-      sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
   # Test trained model
+  # 利用测试集，对比每组样例最大值（输出）是否相等，是的话返回1，得到所有的判断的一个向量，计算正确的百分比就可以得到正确率
   correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  print(sess.run(accuracy,
-                 feed_dict={x: mnist.test.images,
-                            y_: mnist.test.labels}))
-  sess.close()
-
+  print("OCR模型判断的准确率为：",sess.run(accuracy, feed_dict={x: mnist.test.images,
+                                      y_: mnist.test.labels}))
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--data_dir',
-      type=str,
-      default='/tmp/tensorflow/mnist/input_data',
-      help='Directory for storing input data')
-  parser.add_argument(
-      '--xla', type=bool, default=True, help='Turn xla via JIT on')
+  parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
+                      help='Directory for storing input data')
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
